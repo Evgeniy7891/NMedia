8c8bd4fd4d9aa813f47fee6170835501d2e2a4ae
Evgeniy7891 <john2331832@gmail.com>
2022-07-10 17:45:41 +0500

    create RecyclerView, onShareListener

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
@@ -0,0 +1,65 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.formatCount
+import java.security.Provider
+
+typealias OnLikeListener = (post: Post) -> Unit
+typealias OnShareListener = (post: Post) -> Unit
+
+class PostAdapter(
+    private val onLikeListener: OnLikeListener,
+    private val onShareListener: OnShareListener
+) : ListAdapter<Post, PostAdapter.PostViewHolder>(PostDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onLikeListener, onShareListener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = getItem(position)
+        holder.bind(post)
+    }
+
+    class PostViewHolder(
+        private val binding: CardPostBinding,
+        private val onLikeListener: OnLikeListener,
+        private val onShareListener: OnShareListener
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(post: Post) {
+            binding.apply {
+                titleAuthor.text = post.author
+                timePosts.text = post.time
+                textContent.text = post.text
+                textCountLikes.text = formatCount(post.likeCounter)
+                textCountShare.text = formatCount(post.shareCounter)
+                if (post.liked) {
+                    imageLikes.setImageResource(R.drawable.ic_baseline_full_favorite_24)
+                } else {
+                    imageLikes.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+                }
+                imageLikes.setOnClickListener { onLikeListener(post) }
+                imageShare.setOnClickListener { onShareListener(post) }
+            }
+        }
+    }
+}
+
+class PostDiffCallback : DiffUtil.ItemCallback<Post>() {
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
+    }
+
+}
--- app/src/main/java/ru/netology/nmedia/repository/InMemoryPostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/InMemoryPostRepository.kt
@@ -5,29 +5,81 @@ import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
 
 class InMemoryPostRepository : PostRepository { // реализует интерфейс и хранить данные в памяти
-    var post = Post(
-        id = 1,
-        author = "Нетология. Университет интернет-профессий будующего!",
-        logoAvatar = "",
-        time = "21 мая в 18:36",
-        text = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетнгу. Затем появились курсы по дизайну, разработке, аналитике и управдению. Мы растем сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остается с нами: мы верим, что в каждом есть сила, которая заставляет хотеть больше, целиться выше, быжать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку переменю - http://netolo.gy/fyb"
-    )
-    private val data = MutableLiveData(post) // переменная для хранения подписки
-    override fun get(): LiveData<Post> = data // возвращает подписку на пост
-    override fun like() {
-        // post.liked = !post.liked
-        // post.likeCounter = if (post.liked) post.likeCounter + 1 else post.likeCounter - 1
-        post = post.copy(
-            liked = !post.liked,
-            likeCounter = if (post.liked) post.likeCounter - 1 else post.likeCounter + 1
+    private var posts = listOf(
+        Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будующего!",
+            time = "21 мая в 18:36",
+            text = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетнгу. Затем появились курсы по дизайну, разработке, аналитике и управдению. Мы растем сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остается с нами: мы верим, что в каждом есть сила, которая заставляет хотеть больше, целиться выше, быжать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку переменю - http://netolo.gy/fyb"
+        ),
+        Post(
+            id = 2,
+            author = "Нетология. Университет интернет-профессий будующего!",
+            time = "18 сентября в 10:12",
+            text = "Знаний хватит на все: на следующей неделе разбираемся с разработкой мобильных приложений, учимся рассказывать истории и составлять PR-стратегию прямо на бесплатных занятиях \\uD83D\\uDC47\""
+        ),
+        Post(
+            id = 3,
+            author = "Нетология. Университет интернет-профессий будующего!",
+            time = "29 сентября в 10:29",
+            text = "Языков программирования много, и выбрать какой-то один бывает нелегко. Собрали подборку статей, которая поможет вам начать, если вы остановили свой выбор на JavaScript.",
+            likeCounter = 123,
+            shareCounter = 55
+        ),
+        Post(
+            id = 4,
+            author = "Нетология. Университет интернет-профессий будующего!",
+            time = "30 сентября 12:05",
+            text = "Большая афиша мероприятий осени: конференции, выставки и хакатоны для жителей Москвы, Ульяновска и Новосибирска \\uD83D\\uDE09",
+            likeCounter = 123094,
+            shareCounter = 123123123,
+            liked = true
+        ),
+        Post(
+            id = 5,
+            author = "Нетология. Университет интернет-профессий будующего!",
+            time = "1 октября 22:22",
+            text = "Диджитал давно стал частью нашей жизни: мы общаемся в социальных сетях и мессенджерах, заказываем еду, такси и оплачиваем счета через приложения.",
+            likeCounter = 14,
+            shareCounter = 145,
+            liked = true
         )
-        data.value = post // передать обновленные данные
+    )
+    private val data = MutableLiveData(posts) // переменная для хранения подписки
+    override fun getAll(): LiveData<List<Post>> = data // возвращает подписку на пост
+    override fun like() {
+        posts = posts.map {
+            it.copy(
+                liked = !it.liked,
+                likeCounter = if (it.liked) it.likeCounter - 1 else it.likeCounter + 1
+            )
+        }
+        data.value = posts // передать обновленные данные
     }
 
     override fun share() {
-        post = post.copy(
-            shareCounter = post.shareCounter + 1
-        )
-        data.value = post
+        posts = posts.map {
+            it.copy(shareCounter = it.shareCounter + 1)
+        }
+        data.value = posts
+    }
+
+    override fun likeById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                liked = !it.liked,
+                likeCounter = if (!it.liked) it.likeCounter + 1 else it.likeCounter - 1
+            )
+        }
+        data.value = posts
+    }
+
+    override fun shareById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                shareCounter = it.shareCounter + 1
+            )
+        }
+        data.value = posts
     }
 }
